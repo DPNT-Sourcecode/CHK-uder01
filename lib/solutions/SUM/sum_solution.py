c6fcf0cacba08ae7228d73f6dc77a9151396b397
sbx_user1051 <sbx_user1051@169.254.69.189>
2025-08-12 11:55:53 +0000

    Tue Aug 12 11:55:53 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,8 @@
 
 class SumSolution:
-    
-    def compute(self, x, y):
-        raise NotImplementedError()
+    def compute(self, x, y=None):
+        # If the first arg is iterable (list/tuple), unpack it
+        if y is None and hasattr(x, '__iter__'):
+            x, y = x
+        return x + y
+
